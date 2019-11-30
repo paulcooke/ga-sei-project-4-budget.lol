47eab99d210694f828447cb09dec9236578a5e7c
Paul Cooke <paulgfcooke@gmail.com>
2019-11-30 14:33:13 +0000

    attempting fix for 403 errors that are happening on the deployed version only

--- jwt_auth/authentication.py
+++ jwt_auth/authentication.py
@@ -14,6 +14,9 @@ class JWTAuthentication(BasicAuthentication):
         if not header:
             return None
 
+        if header.startswith('Basic'):
+            return None
+
         if not header.startswith('Bearer'):
             raise PermissionDenied({'message': 'Invalid Authorization header'})
 
